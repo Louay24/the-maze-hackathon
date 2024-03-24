cf685104f79ba0b2f204fec15313a74c18e4b523
louay24 <loueyzouaoui@takiacademyteam.com>
2024-03-24 17:28:40 +0100

    husky configured

--- src/constants/antdConfig.ts
+++ src/constants/antdConfig.ts
@@ -1,15 +1,20 @@
 import { ThemeConfig } from 'antd';
+
 import { useEffect, useState } from 'react';
+
 const useThemeConfig = () => {
   const [themeConfig, setThemeConfig] = useState<ThemeConfig>();
+
   useEffect(() => {
     const variables = document.querySelector('.App')?.computedStyleMap();
+
     const themeConfig = {
       token: {
         fontFamily: 'Manrope, sans serif',
         sizeUnit: 10,
         fontSize: 20,
       },
+
       components: {
         Button: {
           borderRadius: 16,
@@ -18,13 +23,16 @@ const useThemeConfig = () => {
           colorPrimary: (variables?.get('--royal-blue') as string[])[0],
         },
       },
+
       Progress: {
         colorBgContainer: (variables?.get('--royal-blue') as string[])[0],
       },
+
       Input: {
         controlHeightLG: 78,
       },
     };
+
     setThemeConfig(themeConfig);
   }, []);
 
