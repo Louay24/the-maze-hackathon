9a692e8262ad4317c038e19940d8f25ee77a0ce0
ouss223 <oussama.ghazouani@insat.ucar.tn>
2024-03-26 01:40:02 +0100

    fixed stylings and added the picture to assets

--- src/features/Registration/StartNow/StartNow.tsx
+++ src/features/Registration/StartNow/StartNow.tsx
@@ -4,16 +4,15 @@ import { RightArrow } from 'src/assets/icons/RightArrow/RightArrow';
 import { Button } from 'src/components/Button/Button';
 import { PATHS } from 'src/constants/paths';
 import { FireOutlined } from '@ant-design/icons';
+import people from 'src/assets/images/people.jpeg';
 
 export const StartNow = () => {
-  const url: string =
-    'https://s3-alpha-sig.figma.com/img/d7e3/369f/72762713ba97779b95dce16b30444d27?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aywGBzoOCKaHLTROUg1J-L1ScD~kPL0HpedKKyLeRMRSEeNFqco0oOU0~cDdiiGrJls082zGA72vdASbm28YY~V3shBdrZDKQ5rjdBkAhc2Bdt0ddVl2m3U3tF7iH2XhMNirQRBWRlNHnnARDZy~o6oBRwn~kuUdziHwFEgniY08wJo2u9-7KR6JCTf8806usdUgtr48BitLID4fwbyFKNux-LAjgO-UsCv70zxnoU5OsnzQ86j4aAFq4vQqa22oHBMrSI~66zmlCI3Km~6tbR9ILTBo~gkwP7q1D977Y7SEcZO4q0Bv73-nJn3MIUK5KVoVbMNAZVJlwnCoWZoO5w__';
   const navigate = useNavigate();
   return (
     <div className="start_now-main start_now">
-      <img src={url} alt="start now" className="start_now-pic" />
+      <img src={people} alt="start now" className="start_now-pic" />
       <h1 className="get_started_title start_now-spacer-top">
-        enhance collaboration, and boost productivity
+        Enhance Collaboration, And Boost Productivity
         <FireOutlined />
       </h1>
       <p className="start_now-description start_now-spacer-bottom">
@@ -22,7 +21,7 @@ export const StartNow = () => {
         delegation, and progress tracking for teams of all sizes.
       </p>
 
-      <div className="start_now-layout start_now-spacer-bottom">
+      <div className="start_now-layout fac start_now-spacer-bottom">
         <Button
           onClick={() => message.error('Not yet')}
           className="get_started-navigation-back"
@@ -37,9 +36,9 @@ export const StartNow = () => {
           GetStarted !{<RightArrow />}
         </Button>
       </div>
-      <h4 >
+      <h4  className='start_now-extra'>
         Already have an account?{' '}
-        <a href="#" className="start_now">
+        <a href="#" className="start_now-reference">
           click here
         </a>
       </h4>
