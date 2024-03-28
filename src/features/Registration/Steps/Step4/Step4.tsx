67bdf2c15ad056a3135f72dee51f01b852dc7f93
louay24 <loueyzouaoui@takiacademyteam.com>
2024-03-28 02:27:27 +0100

    working in upload image

--- src/features/Registration/Steps/Step4/Step4.tsx
+++ src/features/Registration/Steps/Step4/Step4.tsx
@@ -1,10 +1,28 @@
+import { RadioButtons } from 'src/components/RadioButtons/RadioButtons';
 import { StepDetails } from '../../components/StepDetails/StepDetails';
-
+const items = [
+  { value: 'Personal productivity', label: 'Personal productivity' },
+  { value: 'IT', label: 'IT' },
+  { value: 'Design', label: 'Design' },
+  { value: 'Marketing & advertising', label: 'Marketing & advertising' },
+  { value: 'Education', label: 'Education' },
+  { value: 'E-commerce', label: 'E-commerce' },
+  { value: 'Engineering', label: 'Engineering' },
+  { value: 'Other', label: 'Other' },
+];
 export const Step4 = () => {
   return (
-    <StepDetails
-      title="Choose your work field"
-      description="Choose the field where you will use Wayzello"
-    />
+    <div className="step_radio-container">
+      <StepDetails
+        title="Choose your work field"
+        description="Choose the field where you will use Wayzello"
+      />
+      <RadioButtons
+        onChange={(value) => {
+          console.log(value);
+        }}
+        items={items}
+      />
+    </div>
   );
 };
