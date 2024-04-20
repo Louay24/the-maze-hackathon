a5828a66ce29e380f0fbe89c46ef7b9af036048a
louay24 <loueyzouaoui@takiacademyteam.com>
2024-04-20 23:34:34 +0100

    chart

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,10 @@
 import { Route } from './routing';
 
 import { Layout, layoutKey, themes } from 'src/layout/Layout';
-import { DashboardIcon } from 'src/assets/icons/DashboardIcon/DashboardIcon';
 import { Dashboard } from 'src/pages/Dashboard';
 import { Chat } from 'src/pages/Chat';
 import { ChatIcon } from 'src/assets/icons/ChatIcon/ChatIcon';
-import { BrainstormingIcon } from 'src/assets/icons/BrainstormingIcon/BrainstormingIcon';
-import { Brainstorming } from 'src/pages/Brainstorming';
-import { CalendarIcon } from 'src/assets/icons/CalendarIcon/CalendarIcon';
-import { Calendar } from 'src/pages/Calendar';
+
 import { MdOutlineDashboardCustomize } from 'react-icons/md';
 
 import { Number } from 'src/components/Number/Number';
@@ -49,24 +45,7 @@ export const routes: Route[] = [
         icon: <ChatIcon />,
         suffix: <Number color="blue" content="3" />,
       },
-      {
-        path: Paths.Project4,
-        guard: 'private',
-        theme: themes.leaks,
-        showInMenu: true,
-        label: 'brainstorming',
-        component: Brainstorming,
-        icon: <BrainstormingIcon />,
-      },
-      {
-        path: Paths.Project5,
-        guard: 'private',
-        showInMenu: true,
-        theme: themes.leaks,
-        label: 'calendar',
-        component: Calendar,
-        icon: <CalendarIcon />,
-      },
+
       {
         path: Paths.calculator,
         guard: 'public',
