6c26ff96f93800a4437cdb9a51040ce380dc0e8e
iheb-sebai <iyed.se1ai@gmail.com>
2024-04-20 22:04:21 +0100

    add socket manager class

---
+++ src/socket/socketManager.ts
@@ -0,0 +1,79 @@
+import { io, Socket } from 'socket.io-client'
+
+
+class SocketManager{
+  private socket: Socket | null
+  token: string | null
+  private connectedRoomId: string | null
+  private connectedWorkSpaceId: string | null
+
+  constructor() {
+    this.socket = null
+    this.connectedWorkSpaceId = null
+    this.connectedRoomId = null
+    this.token = localStorage.getItem('access_token')
+    this._connectSocket()
+  }
+
+  public _connectSocket(): void {
+    this.connectedRoomId &&
+      console.log('Should Leave from the previous room with ID', this.connectedRoomId)
+    const token = localStorage.getItem('access_token')
+    if (this.socket || !token) return
+
+    const socket = io(import.meta.env.VITE_APP_SOCKET_BASE_URL, {
+      extraHeaders: {
+        authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      },
+    })
+
+    this.socket = socket
+    socket.on('connect', () => {
+      this.socket = socket
+      this._membersManagement()
+    })
+
+    socket.on('error', (e:any) => {
+      console.error('socket error', e)
+    })
+  }
+
+  public _joinRoom(listId: string): void {
+    if (!this.socket) return
+
+    this.socket.emit('joinRoom', listId)
+    this.connectedRoomId = listId
+
+  
+
+    this.socket.on('notifRoom', (update:any) => {
+      this.connectedRoomId = listId
+
+      let index = update.indexOf('//')
+
+      if (index !== -1) {
+        let type = update.substr(0, index)
+        let data = update.substr(index + 2)
+        console.log({ type, data })
+
+      } else {
+        console.error("No occurrence of '//' found in the string.")
+      }
+    })
+  }
+
+  public _membersManagement(): void {
+    if (!this.socket) return
+    this.socket.on('userActive', () => {})
+  }
+
+
+  public _getSocket() {
+    if (this.socket) return this.socket
+    else this._connectSocket()
+  }
+}
+
+const socketManager = new SocketManager()
+
+export default socketManager
