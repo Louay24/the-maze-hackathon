60cbfbc02a7b5d540b8b73427340eed02e7d9ec7
louay24 <loueyzouaoui@takiacademyteam.com>
2024-04-21 01:16:16 +0100

    figma

--- src/pages/Figma.tsx
+++ src/pages/Figma.tsx
@@ -8,6 +8,10 @@ export const Figma = () => {
       challenge: 41,
       link: 'https://www.figma.com/file/695aG6t8q1Dc65EJXPuAR2/Symptom-Tracker?type=design&node-id=0-1&mode=design&t=YMEUCTfuMW5Xzkb8-0',
     },
+    {
+      challenge: 16,
+      link: 'https://www.figma.com/file/ENr7uHO9xGiNFLKntbZnFn/Intuitive-Fitness-Tracking-App-UI-Design?type=design&node-id=0-1&mode=design&t=ZSrH8rncDWsAztLG-0',
+    },
   ];
   return (
     <div style={{ display: 'flex', gap: '1rem' }}>
